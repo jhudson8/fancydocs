abfe959c9cb4f92f79715b8830a5cd683504b266
Joe Hudson <joehud@gmail.com>
2014-07-21 10:02:46 -0400

    fix duplicate params with empty method signature

--- js/models/method.js
+++ js/models/method.js
@@ -5,5 +5,17 @@ module.exports = Backbone.Model.extend({
   },
   viewUrl: function() {
     return (this.project || this.collection.project).viewUrl() + '/method/' + this.id;
+  },
+
+  parse: function(data) {
+    var profiles = data.profiles;
+    if (profiles) {
+      for (var i=0; i<profiles.length; i++) {
+        if (profiles[i] === '()') {
+          profiles[i] = '';
+        }
+      }
+    }
+    return data;
   }
 });
