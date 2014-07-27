454a03dd7c120af39224247aeb08984887f77ff3
Joe Hudson <joehud@gmail.com>
2014-07-27 15:23:49 -0400

    add search filters

--- js/views/nav/side-nav-packages.js
+++ js/views/nav/side-nav-packages.js
@@ -1,6 +1,7 @@
 /** @jsx React.DOM */
 var util = require('./nav-util');
 
+// FIXME use util to DRY up with side-nav-methods
 module.exports = React.createClass({
   mixins: ['modelAware', 'events'],
 
@@ -13,27 +14,71 @@ module.exports = React.createClass({
       apis[apiName] = apis[apiName] || [];
       apis[apiName].push(pkg);
     });
-    var children = _.map(apis, function(packages, apiName) {
-      return {
-        label: apiName,
-        key: apiName,
-        children: packages.map(function(pkg) {
-          return {
-            key: pkg.id,
-            label: pkg.get('name'),
-            model: pkg,
-            type: 'package',
-            icon: util.icons.package
+
+    var self = this;
+    var children = _.chain(apis)
+        .map(function(packages, apiName) {
+          var _children = self.mapChildren(packages, function(pkg) {
+            return {
+              key: pkg.id,
+              label: pkg.get('name'),
+              model: pkg,
+              type: 'package',
+              icon: util.icons.package
+            };
+          });
+
+          return _children && {
+            label: apiName,
+            key: apiName,
+            children: _children
           };
         })
-      };
-    });
+      .compact()
+      .value();
+
     var startingLevel = 1;
     if (children.length === 1) {
       // the api type is assumed since there is only 1
       children = children[0].children;
       startingLevel = 2;
     }
-    return util.projectNavMenu(children, this, this.props.viewState, {snippet: true, allowHilight: true, startingLevel: startingLevel});
+    var menu = util.projectNavMenu(children, this, this.props.viewState, {snippet: true, allowHilight: true, startingLevel: startingLevel});
+    return (
+      <div>
+        <div className="ui nav-search form">
+          <div className="field">
+            <div className="ui icon input">
+              <input type="text" placeholder="Search..." onChange={this.filter} onKeyUp={this.filter}/>
+              <i className="fa-search icon"></i>
+            </div>
+          </div>
+        </div>
+        {menu}
+      </div>
+    );
+  },
+
+  filter: function(e) {
+    var value = $(e.currentTarget).val();
+    this.setState({search: value});
+  },
+
+  mapChildren: function(children, callback) {
+    var self = this;
+    var filter = this.state.search;
+    filter = filter && filter.toLowerCase();
+    children = children.models || children;
+    children = children && _.chain(children)
+        .map(function(child) {
+          var name = child.get && child.get('name');
+          name = name && name.toLowerCase() || '';
+          if (!filter || name.indexOf(filter) >= 0) {
+            return callback.call(self, child);
+          }
+        })
+        .compact()
+        .value() || [];
+    return children.length > 0 && children;
   }
 });
