388209544d4d2808617beddc6669607199b2826d
Joe Hudson <joehud@gmail.com>
2014-07-29 21:38:03 -0400

    add search filters

--- js/views/nav/nav-util.js
+++ js/views/nav/nav-util.js
@@ -2,6 +2,10 @@
 
 var util = require('../../utils/util');
 
+var KEY_UP = 38;
+var KEY_DOWN = 40;
+var KEY_ENTER = 13;
+
 module.exports = {
   icons: {
     package: 'fa-th icon',
@@ -23,8 +27,47 @@ module.exports = {
         {children}
       </div>
     );
+  },
+
+  filter: function(type, self) {
+    return function(e) {
+      if ([38, 40, 13].indexOf(e.keyCode) >= 0) {
+        onSpecialKey.call(self, e.keyCode, type);
+        e.stopPropagation();
+        e.preventDefault();
+      } else {
+        var value = $(e.currentTarget).val();
+        self.setState({search: value});
+      }
+    }
+  },
+
+  filteredNavChildren: function(apis, type, iconType, self) {
+    var children = _.chain(apis)
+        .map(function(collection, apiName) {
+          var _children = mapChildren(collection, function(model) {
+            return {
+              key: model.id,
+              label: model.get('name'),
+              model: model,
+              type: type,
+              icon: module.exports.icons[iconType],
+              id: 'nav-' + model.domId()
+            };
+          }, self);
+
+          return _children && {
+            label: apiName,
+            key: apiName,
+            children: _children
+          };
+        })
+      .compact()
+      .value();
+    return children;
   }
 };
+
 
 function addMenuChildren (navItems, level, children, self, viewState, options) {
   if (!navItems) {
@@ -37,14 +80,14 @@ function addMenuChildren (navItems, level, children, self, viewState, options) {
   _.each(navItems, function(navItem) {
     var model = navItem.model;  
     var hilighted = options.allowHilight && hilightModel && ((hilightModel.isEqual && hilightModel.isEqual(model)) || hilightModel === model);
-    var className = hilighted ? ' active green' : '';
-    var url = navItem.url || model && model.viewUrl(false, !options.snippet);
+    var className = (hilighted ? ' active green ' : '') + (navItem.className || '');
+    var url = navItem.url || model && model.viewUrl && model.viewUrl(false, !options.snippet);
     var onClick = model && (options.snippet ? util.snippetTo(navItem.type, navItem.model, self) : options.jumpTo && util.jumpTo(model, self));
     var icon = navItem.icon && <i className={navItem.icon + ' icon'}/>;
     var label = navItem.label;
     var key = navItem.key;
     var clazz = url ? React.DOM.a : React.DOM.div;
-    var props = {key: key, className: 'item' + className, href: url, onClick: onClick}
+    var props = {id: navItem.id, key: key, className: 'item' + className, href: url, onClick: onClick}
     var navItemChildren = navItem.children;
 
     if (level === 1 && navItemChildren && navItemChildren.length > 0 && navItems.length > 1) {
@@ -113,4 +156,78 @@ function toggle(toggleId, model, self) {
     state.toggleState[toggleId] = !state.toggleState[toggleId];
     self.forceUpdate();
   }
+}
+
+
+function onSpecialKey(keyCode, type) {
+  var keySelected = this.state.keySelected;
+  var children = this.state.children;
+  var previous, current, next, first;
+  function iterate(children, wasCurrent) {
+    if (!children) return;
+    var child;
+    for (var i=0; i<children.length && !next; i++) {
+      child = children[i];
+      if (!first && child.model) {
+        first = child;
+      }
+      if (child.model && current) {
+        next = child;
+        return;
+      }
+      if (child.model && child.model === keySelected) {
+        current = child;
+      } else {
+        if (child.model) {
+          previous = child;
+        }
+        if (child.children) {
+          iterate(child.children);
+        }
+      }
+    }
+  }
+  iterate(this.state.children);
+
+  var el = $(this.getDOMNode());
+  if (keyCode === KEY_UP) {
+    keySelected = previous && previous.model;
+  } else if (keyCode === KEY_DOWN) {
+    if (!current) {
+      keySelected = first && first.model;
+    } else {
+      keySelected = next && next.model;
+    }
+  } else if (keyCode === KEY_ENTER) {
+    el.find('.keycode-hilight').removeClass('keycode-hilight');
+    if (current) {
+      Backbone.history.navigate(current.model.viewUrl(true), {replace: false, trigger: false});
+      this.props.onSnippetTo(type, current.model);
+    }
+  }
+
+  el.find('.keycode-hilight').removeClass('keycode-hilight');
+  if (keySelected) {
+    var domId = keySelected.domId();
+    el.find('#' + 'nav-' + domId).addClass('keycode-hilight');
+  }
+  this.state.keySelected = keySelected;
+}
+
+
+function mapChildren(children, callback, self) {
+  var filter = self.state.search;
+  filter = filter && filter.toLowerCase();
+  children = children.models || children;
+  children = children && _.chain(children)
+      .map(function(child) {
+        var name = child.get && child.get('name');
+        name = name && name.toLowerCase() || '';
+        if (!filter || name.indexOf(filter) >= 0) {
+          return callback.call(self, child);
+        }
+      })
+      .compact()
+      .value() || [];
+  return children.length > 0 && children;
 }
--- js/views/nav/side-nav-packages.js
+++ js/views/nav/side-nav-packages.js
@@ -5,6 +5,10 @@ var util = require('./nav-util');
 module.exports = React.createClass({
   mixins: ['modelAware', 'events'],
 
+  getInitialState: function() {
+    return {};
+  },
+
   render: function() {
     var project = this.getModel();
     var apis = {};
@@ -14,28 +18,7 @@ module.exports = React.createClass({
       apis[apiName] = apis[apiName] || [];
       apis[apiName].push(pkg);
     });
-
-    var self = this;
-    var children = _.chain(apis)
-        .map(function(packages, apiName) {
-          var _children = self.mapChildren(packages, function(pkg) {
-            return {
-              key: pkg.id,
-              label: pkg.get('name'),
-              model: pkg,
-              type: 'package',
-              icon: util.icons.package
-            };
-          });
-
-          return _children && {
-            label: apiName,
-            key: apiName,
-            children: _children
-          };
-        })
-      .compact()
-      .value();
+    var children = util.filteredNavChildren(apis, 'packages', 'package', this);
 
     var startingLevel = 1;
     if (children.length === 1) {
@@ -43,13 +26,16 @@ module.exports = React.createClass({
       children = children[0].children;
       startingLevel = 2;
     }
+    this.state.children = children;
+
     var menu = util.projectNavMenu(children, this, this.props.viewState, {snippet: true, allowHilight: true, startingLevel: startingLevel});
+    var filter = util.filter('package', this);
     return (
       <div>
         <div className="ui nav-search form">
           <div className="field">
             <div className="ui icon input">
-              <input type="text" placeholder="Search..." onChange={this.filter} onKeyUp={this.filter}/>
+              <input type="text" ref="search" placeholder="Search..." onChange={filter} onKeyUp={filter}/>
               <i className="fa-search icon"></i>
             </div>
           </div>
@@ -59,26 +45,7 @@ module.exports = React.createClass({
     );
   },
 
-  filter: function(e) {
-    var value = $(e.currentTarget).val();
-    this.setState({search: value});
-  },
-
-  mapChildren: function(children, callback) {
-    var self = this;
-    var filter = this.state.search;
-    filter = filter && filter.toLowerCase();
-    children = children.models || children;
-    children = children && _.chain(children)
-        .map(function(child) {
-          var name = child.get && child.get('name');
-          name = name && name.toLowerCase() || '';
-          if (!filter || name.indexOf(filter) >= 0) {
-            return callback.call(self, child);
-          }
-        })
-        .compact()
-        .value() || [];
-    return children.length > 0 && children;
+  componentDidMount: function() {
+    $(this.refs.search.getDOMNode()).focus();
   }
 });
