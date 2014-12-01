60526d359b3661c695b8bf17ead43636625a3d4d
Joe Hudson <joehud@gmail.com>
2014-11-30 21:34:48 -0500

    sync

--- app.js
+++ app.js
@@ -16075,7 +16075,7 @@
 	          React.DOM.a({href: 'https://github.com/' + pkg.project.get('repo') + '/' + pkg.project.get('name'), className: "ui label"}, 
 	            React.DOM.i({className: "large github alternate icon"})
 	          ), 
-	          pkg.project.get('repo') + '/' + pkg.project.get('title')
+	          React.DOM.span({className: "hide-mobile"}, pkg.project.get('repo'), " / "), " ", pkg.project.get('title')
 	        )
 	      );
 
@@ -16244,7 +16244,7 @@
 	          React.DOM.a({href: 'https://github.com/' + project.get('repo') + '/' + project.get('name'), className: "ui label"}, 
 	            React.DOM.i({className: "large github alternate icon"})
 	          ), 
-	          project.get('repo') + '/' + project.get('title')
+	          React.DOM.span({className: "hide-mobile"}, project.get('repo'), " / "), " ", project.get('title')
 	        ), 
 
 	        Markdown({tag: "p", body: project.get('summary')}), 
@@ -36860,7 +36860,7 @@
 	var SyntheticUIEvent = __webpack_require__(196);
 	var ViewportMetrics = __webpack_require__(167);
 
-	var getEventModifierState = __webpack_require__(204);
+	var getEventModifierState = __webpack_require__(205);
 
 	/**
 	 * @interface MouseEvent
@@ -36954,7 +36954,7 @@
 	"use strict";
 
 	var CSSPropertyOperations = __webpack_require__(125);
-	var DOMChildrenOperations = __webpack_require__(205);
+	var DOMChildrenOperations = __webpack_require__(204);
 	var DOMPropertyOperations = __webpack_require__(69);
 	var ReactMount = __webpack_require__(81);
 	var ReactPerf = __webpack_require__(83);
@@ -37969,7 +37969,7 @@
 	var SyntheticUIEvent = __webpack_require__(196);
 
 	var getEventKey = __webpack_require__(206);
-	var getEventModifierState = __webpack_require__(204);
+	var getEventModifierState = __webpack_require__(205);
 
 	/**
 	 * @interface KeyboardEvent
@@ -38111,7 +38111,7 @@
 
 	var SyntheticUIEvent = __webpack_require__(196);
 
-	var getEventModifierState = __webpack_require__(204);
+	var getEventModifierState = __webpack_require__(205);
 
 	/**
 	 * @interface TouchEvent
@@ -38743,64 +38743,6 @@
 /* 204 */
 /***/ function(module, exports, __webpack_require__) {
 
-	/** @jsx React.DOM *//**
-	 * Copyright 2013 Facebook, Inc.
-	 *
-	 * Licensed under the Apache License, Version 2.0 (the "License");
-	 * you may not use this file except in compliance with the License.
-	 * You may obtain a copy of the License at
-	 *
-	 * http://www.apache.org/licenses/LICENSE-2.0
-	 *
-	 * Unless required by applicable law or agreed to in writing, software
-	 * distributed under the License is distributed on an "AS IS" BASIS,
-	 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	 * See the License for the specific language governing permissions and
-	 * limitations under the License.
-	 *
-	 * @providesModule getEventModifierState
-	 * @typechecks static-only
-	 */
-
-	"use strict";
-
-	/**
-	 * Translation from modifier key to the associated property in the event.
-	 * @see http://www.w3.org/TR/DOM-Level-3-Events/#keys-Modifiers
-	 */
-
-	var modifierKeyToProp = {
-	  'Alt': 'altKey',
-	  'Control': 'ctrlKey',
-	  'Meta': 'metaKey',
-	  'Shift': 'shiftKey'
-	};
-
-	// IE8 does not implement getModifierState so we simply map it to the only
-	// modifier keys exposed by the event itself, does not support Lock-keys.
-	// Currently, all major browsers except Chrome seems to support Lock-keys.
-	function modifierStateGetter(keyArg) {
-	  /*jshint validthis:true */
-	  var syntheticEvent = this;
-	  var nativeEvent = syntheticEvent.nativeEvent;
-	  if (nativeEvent.getModifierState) {
-	    return nativeEvent.getModifierState(keyArg);
-	  }
-	  var keyProp = modifierKeyToProp[keyArg];
-	  return keyProp ? !!nativeEvent[keyProp] : false;
-	}
-
-	function getEventModifierState(nativeEvent) {
-	  return modifierStateGetter;
-	}
-
-	module.exports = getEventModifierState;
-
-
-/***/ },
-/* 205 */
-/***/ function(module, exports, __webpack_require__) {
-
 	/* WEBPACK VAR INJECTION */(function(process) {/** @jsx React.DOM *//**
 	 * Copyright 2013-2014 Facebook, Inc.
 	 *
@@ -38981,6 +38923,64 @@
 	module.exports = DOMChildrenOperations;
 	
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(65)))
+
+/***/ },
+/* 205 */
+/***/ function(module, exports, __webpack_require__) {
+
+	/** @jsx React.DOM *//**
+	 * Copyright 2013 Facebook, Inc.
+	 *
+	 * Licensed under the Apache License, Version 2.0 (the "License");
+	 * you may not use this file except in compliance with the License.
+	 * You may obtain a copy of the License at
+	 *
+	 * http://www.apache.org/licenses/LICENSE-2.0
+	 *
+	 * Unless required by applicable law or agreed to in writing, software
+	 * distributed under the License is distributed on an "AS IS" BASIS,
+	 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	 * See the License for the specific language governing permissions and
+	 * limitations under the License.
+	 *
+	 * @providesModule getEventModifierState
+	 * @typechecks static-only
+	 */
+
+	"use strict";
+
+	/**
+	 * Translation from modifier key to the associated property in the event.
+	 * @see http://www.w3.org/TR/DOM-Level-3-Events/#keys-Modifiers
+	 */
+
+	var modifierKeyToProp = {
+	  'Alt': 'altKey',
+	  'Control': 'ctrlKey',
+	  'Meta': 'metaKey',
+	  'Shift': 'shiftKey'
+	};
+
+	// IE8 does not implement getModifierState so we simply map it to the only
+	// modifier keys exposed by the event itself, does not support Lock-keys.
+	// Currently, all major browsers except Chrome seems to support Lock-keys.
+	function modifierStateGetter(keyArg) {
+	  /*jshint validthis:true */
+	  var syntheticEvent = this;
+	  var nativeEvent = syntheticEvent.nativeEvent;
+	  if (nativeEvent.getModifierState) {
+	    return nativeEvent.getModifierState(keyArg);
+	  }
+	  var keyProp = modifierKeyToProp[keyArg];
+	  return keyProp ? !!nativeEvent[keyProp] : false;
+	}
+
+	function getEventModifierState(nativeEvent) {
+	  return modifierStateGetter;
+	}
+
+	module.exports = getEventModifierState;
+
 
 /***/ },
 /* 206 */
