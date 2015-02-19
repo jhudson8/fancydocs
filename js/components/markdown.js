35c3be6a4cb23a465d80921276c7046369b591ee
Joe Hudson <joehud@gmail.com>
2015-02-19 13:54:51 -0500

    add syntax hilighting

--- js/components/markdown.js
+++ js/components/markdown.js
@@ -1,6 +1,20 @@
 /** @jsx React.DOM */
 
-var converter = new Showdown.converter();
+var marked = require('marked');
+var options = {
+  renderer: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: true,
+  smartLists: true,
+  smartypants: false,
+  highlight: function (code) {
+    return require('highlight.js').highlightAuto(code).value;
+  }
+};
+marked.setOptions(options);
 
 module.exports = React.createClass({
   getInitialState: function() {
@@ -10,8 +24,8 @@ module.exports = React.createClass({
   },
   render: function() {
     var Container = React.DOM[this.props.tag || 'div'];
-    var html = converter.makeHtml(
-      this.props.body || (this.props.children && this.props.children.toString()) || '').replace(/id=\"[^"]*"/g, '');
+    var html = marked(
+      this.props.body || (this.props.children && this.props.children.toString()) || '', options).replace(/id=\"[^"]*"/g, '');
     
     return (
       <Container key={this.state.id} id={this.props.id}>
